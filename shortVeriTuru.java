2d9b00264464720cf1ee060ce93e4ce4e5576787
Kupawesta <84570769+Kupawesta@users.noreply.github.com>
2021-12-19 13:20:32 +0300

    Add files via upload

---
+++ shortVeriTuru.java
@@ -0,0 +1,16 @@
+package com.Veli.veriturleri;
+// Short 16 bit büyüklüðündedir. aralýðý -32768/32767
+public class shortVeriTuru {
+
+	public static void main(String[] args) {
+		short shortsayi = 8;
+		System.out.println("deðer = " + shortsayi);
+		
+		short maximumshortdegeri = Short.MAX_VALUE;
+		short minimumshortdegeri = Short.MIN_VALUE;
+		
+		System.out.println(maximumshortdegeri);
+		System.out.println(minimumshortdegeri);
+	}
+
+}
